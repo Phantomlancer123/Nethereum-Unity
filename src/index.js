e7055d1f6f8cfb47f91db04364041fec23a87104
Mr Dev <mrdev0803@gmail.com>
2022-04-11 13:29:50 +0200

    remove neverused

--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import { Web3ReactProvider } from '@web3-react/core'
 import { ModalProvider } from './context/modal-context'
-import { Web3Provider } from "@ethersproject/providers";
-
-function getLibrary(provider) {
-
-  return new Web3Provider(provider);
-
-}
 
 ReactDOM.render(
   <React.StrictMode>
